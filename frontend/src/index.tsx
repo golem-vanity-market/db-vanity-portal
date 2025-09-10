7f16a6316277bc3321f853dc408876d03300a780
scx1332 <sieciech@gmail.com>
2025-09-10 17:07:00 +0200

    Backend for serving provider data

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import Dashboard from "./Dashboard";
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
+//vite env
+
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"dashboard"}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Dashboard />
     </BrowserRouter>
   </React.StrictMode>,
