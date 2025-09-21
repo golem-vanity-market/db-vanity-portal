a1ba31efbe2d57412fa5f671943aa45cdb9b9738
sieci <sieciech@gmail.com>
2025-09-21 21:55:31 +0200

    disable polyfills

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
     compression(),
-    nodePolyfills({
+/*    nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
-    }),
+    }),*/
   ],
   base: process.env.VITE_BASE || "/",
   define: {
