81afe7a1227410b4e03a053511c9371c7cce0c45
scx1332 <sieciech@gmail.com>
2025-09-22 13:22:54 +0200

    remove ethers

--- frontend/src/provider/DetailsPage.tsx
+++ frontend/src/provider/DetailsPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import { getAddress } from "ethers";
+import { getAddress } from "viem";
 
 interface FancyEntry {
   address: string;
