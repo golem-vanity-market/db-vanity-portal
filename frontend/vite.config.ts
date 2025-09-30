6273d3ac7e2b26c399b3283d937972031b5b1979
scx1332 <sieciech@gmail.com>
2025-09-30 12:03:00 +0200

    Fix how .env is read in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression2";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,36 +6,39 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
 
 // https://vitejs.dev/config https://vitest.dev/config
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-    tailwindcss(),
-    compression(),
-    nodePolyfills({
-      // Whether to polyfill `node:` protocol imports.
-      include: ["buffer"],
-    }),
-  ],
-  base: process.env.VITE_BASE || "/",
-  build: {
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id.toString().split("node_modules/")[1].split("/")[0].toString();
-          }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: ["babel-plugin-react-compiler"],
+        },
+      }),
+      tailwindcss(),
+      compression(),
+      nodePolyfills({
+        // Whether to polyfill `node:` protocol imports.
+        include: ["buffer"],
+      }),
+    ],
+    base: env.VITE_BASE || "/",
+    build: {
+      chunkSizeWarningLimit: 1500,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
